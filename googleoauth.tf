7fadc50c8f9de53968ba5a4531aa079f66592289
Hattmo <matthew@hattmo.com>
2020-04-12 23:01:16 -0500

    value to output vars

--- googleoauth.tf
+++ googleoauth.tf
@@ -2,9 +2,11 @@ variable "CLIENT_ID" {}
 variable "CLIENT_SECRET" {}
 
 output "client_id" {
+    value = var.CLIENT_ID
     sensitive = true
 }
 
 output "client_secret" {
+    value = var.CLIENT_SECRET
     sensitive = true
 }
